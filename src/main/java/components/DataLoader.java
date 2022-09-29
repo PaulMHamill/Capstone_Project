e7c74f00d740c6c49ac22064367c6c80cbc9ad28
paulhamill <enquiries@nestboxproperty.co.uk>
2022-09-29 15:38:14 +0100

    @Autowired added to DataLoader

--- src/main/java/components/DataLoader.java
+++ src/main/java/components/DataLoader.java
@@ -1,9 +1,14 @@
 package components;
 
+import models.Guest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import repositories.GuestRepository;
+import repositories.PodRepository;
+import repositories.ReservationRepository;
 
 import java.text.ParseException;
 
@@ -11,10 +16,22 @@ import java.text.ParseException;
 @Component
 public class DataLoader implements ApplicationRunner {
 
+    @Autowired
+    GuestRepository guestRepository;
+    @Autowired
+    PodRepository podRepository;
+    @Autowired
+    ReservationRepository reservationRepository;
+
     public DataLoader() {
     }
 
     public void run(ApplicationArguments args) throws ParseException {
+
+        Guest guest1 = new Guest("Paul", "Hamill", "PH@gmail.com");
+        GuestRepository.save(Guest);
+
+
 
     }
 }
