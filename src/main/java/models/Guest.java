f608cb10505a477314ae9385626911fb8af785aa
paulhamill <enquiries@nestboxproperty.co.uk>
2022-09-29 13:47:26 +0100

    Guest class created

--- src/main/java/models/Guest.java
+++ src/main/java/models/Guest.java
@@ -1,4 +1,38 @@
 package models;
 
 public class Guest {
+
+    private String firstName;
+    private String secondName;
+    private String email;
+
+    public Guest(String firstName, String secondName, String email) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
