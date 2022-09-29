5da278661ae4301ff0351abdef7c2c469dc26c09
paulhamill <enquiries@nestboxproperty.co.uk>
2022-09-29 15:10:33 +0100

    @repository added to repositories

--- src/main/java/repositorys/PodRepository.java
+++ src/main/java/repositorys/PodRepository.java
@@ -2,6 +2,8 @@ package repositorys;
 
 import models.Pod;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PodRepository extends JpaRepository<Pod, Long> {
 }
