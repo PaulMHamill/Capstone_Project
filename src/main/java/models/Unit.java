edd6045aea774e394d9c850af0a772d4f477d200
paulhamill <enquiries@nestboxproperty.co.uk>
2022-09-29 14:03:51 +0100

    abstract unit class created

---
+++ src/main/java/models/Unit.java
@@ -0,0 +1,54 @@
+package models;
+
+import java.util.ArrayList;
+
+public abstract class Unit {
+
+    private int capacity;
+    private ArrayList<Guest> guests;
+
+    public Unit(int capacity) {
+        this.capacity = capacity;
+        this.guests = new ArrayList<Guest>();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public ArrayList<Guest> getGuests() {
+        return guests;
+    }
+
+    public void setGuests(ArrayList<Guest> guests) {
+        this.guests = guests;
+    }
+
+    public int guestListSize(){
+        return this.guests.size();
+    }
+
+    public void checkInGuest(Guest guest) {
+        if (this.guestListSize()< this.capacity){
+            this.guests.add(guest);
+        }
+    }
+
+    public void checkOutGuests() {
+        if (this.isOccupied()) {
+            this.guests.clear();
+        }
+    }
+
+    public boolean isOccupied() {
+        return this.guestListSize() > 0;
+    }
+
+    public boolean isVacant() {
+        return this.guestListSize() == 0;
+    }
+}
