8b74eb2003218c324883fd388385520a7e3a1f7c
paulhamill <enquiries@nestboxproperty.co.uk>
2022-09-29 15:08:16 +0100

    Guest repository added

--- src/main/java/repositorys/GuestRepository.java
+++ src/main/java/repositorys/GuestRepository.java
@@ -1,4 +1,7 @@
 package repositorys;
 
-public interface GuestRepository {
+import models.Guest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuestRepository extends JpaRepository<Guest, Long> {
 }
