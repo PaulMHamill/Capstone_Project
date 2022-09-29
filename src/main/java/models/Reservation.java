e44577c60d9228a5f21e415f1350614b69ba4be3
paulhamill <enquiries@nestboxproperty.co.uk>
2022-09-29 14:23:21 +0100

    Resevation class added

--- src/main/java/models/Reservation.java
+++ src/main/java/models/Reservation.java
@@ -1,4 +1,32 @@
 package models;
 
 public class Reservation {
+
+    private int numberOfNights;
+    private Pod pod;
+
+    public Reservation(int numberOfNights, Pod pod) {
+        this.numberOfNights = numberOfNights;
+        this.pod = pod;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public Pod getPod() {
+        return pod;
+    }
+
+    public void setPod(Pod pod) {
+        this.pod = pod;
+    }
+
+    public double getTotalForStay() {
+        return this.numberOfNights * this.pod.getNightlyRate();
+    }
 }
