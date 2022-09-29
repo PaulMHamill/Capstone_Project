f2251304a36c4346558532c1e577d64f7794c67d
paulhamill <enquiries@nestboxproperty.co.uk>
2022-09-29 13:59:53 +0100

    abstract pod class created

--- src/main/java/models/Pod.java
+++ src/main/java/models/Pod.java
@@ -1,4 +1,54 @@
 package models;
 
-public class Pod {
+import java.util.ArrayList;
+
+public abstract class Pod {
+
+    private int capacity;
+    private ArrayList<Guest> guests;
+
+    public Pod(int capacity) {
+        this.capacity = capacity;
+        this.guests = new ArrayList<Guest>();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public ArrayList<Guest> getGuests() {
+        return guests;
+    }
+
+    public void setGuests(ArrayList<Guest> guests) {
+        this.guests = guests;
+    }
+
+    public int guestListSize(){
+        return this.guests.size();
+    }
+
+    public void checkInGuest(Guest guest) {
+        if (this.guestListSize()< this.capacity){
+            this.guests.add(guest);
+        }
+    }
+
+    public void checkOutGuests() {
+        if (this.isOccupied()) {
+            this.guests.clear();
+        }
+    }
+
+    public boolean isOccupied() {
+        return this.guestListSize() > 0;
+    }
+
+    public boolean isVacant() {
+        return this.guestListSize() == 0;
+    }
 }
