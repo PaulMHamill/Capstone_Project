c51dc7201887adc5d61fc23f444735ad2d72701b
paulhamill <enquiries@nestboxproperty.co.uk>
2022-09-30 13:42:32 +0100

    reservation controller created

--- src/main/java/com/codeclan/example/bookingservice/controller/ReservationController.java
+++ src/main/java/com/codeclan/example/bookingservice/controller/ReservationController.java
@@ -1,7 +1,24 @@
 package com.codeclan.example.bookingservice.controller;
 
+import com.codeclan.example.bookingservice.models.Reservation;
+import com.codeclan.example.bookingservice.repositories.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReservationController {
+
+    @Autowired
+    ReservationRepository reservationRepository;
+
+    @GetMapping("/reservations")
+    public ResponseEntity<Reservation> postReservations(@RequestBody Reservation reservation) {
+        reservationRepository.save(reservation);
+        return new ResponseEntity<>(reservation, HttpStatus.CREATED);
+    }
 }
