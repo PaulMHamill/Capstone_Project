ae5a266db5ddf71449216931c8e11d2d8e76440d
paulhamill <enquiries@nestboxproperty.co.uk>
2022-10-02 14:58:13 +0100

    pending payment class added to reservation process

---
+++ src/main/java/com/codeclan/example/bookingservice/reservationprocess/PaymentStatus.java
@@ -0,0 +1,9 @@
+package com.codeclan.example.bookingservice.reservationprocess;
+
+/**
+ * These would mirror payment API vendor responses. Accepted and Declined are used to determine if the payment
+ * went through.
+ */
+public enum PaymentStatus {
+    Accepted, Declined
+}
