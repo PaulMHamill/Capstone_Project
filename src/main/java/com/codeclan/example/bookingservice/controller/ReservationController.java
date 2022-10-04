7deaffcb84158055460280f2350fe01920b5cccc
paulhamill <enquiries@nestboxproperty.co.uk>
2022-10-04 13:23:54 +0100

    reservation controller updated

--- src/main/java/com/codeclan/example/bookingservice/controller/ReservationController.java
+++ src/main/java/com/codeclan/example/bookingservice/controller/ReservationController.java
@@ -57,6 +57,16 @@ public class ReservationController {
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
+    @PutMapping(value="/reservations/{id}")
+    public ResponseEntity<Reservation> putReservation(@RequestBody Reservation reservation, @PathVariable Long id){
+        Reservation reservationToUpdate = reservationRepository.findById(id).get();
+        reservationToUpdate.setDates(reservation.getDates());
+        reservationToUpdate.setPod(reservation.getPod());
+        reservationToUpdate.setGuest(reservation.getGuest());
+        reservationRepository.save(reservationToUpdate);
+        return new ResponseEntity<>(reservationToUpdate, HttpStatus.OK);
+    }
+
     @ModelAttribute("reservationProcess")
     public ReservationForm getReservationForm() {
         return new ReservationForm();
